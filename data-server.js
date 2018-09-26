400f70704a8041f453700c9db0af177ec1a0cf8f
Vincent <vincent@Vincents-Mac-mini.local>
2018-09-26 18:30:44 -0400

    updated paths

---
+++ data-server.js
@@ -0,0 +1,78 @@
+var fs = require('fs');
+var path = require('path');
+//Global arrays to contain employee and department data
+//
+var employees   = [];
+var departments = [];
+
+//Blocking Helper function to load a array
+//      inputs: file : name of .json file
+//          reject : Promise reject function
+function readFile(file, reject){
+//    console.log('reading '+file);
+    try {
+        return  JSON.parse( fs.readFileSync(path.join(__dirname, '/views/'+ file +'.json'), 'utf8'));
+    } catch (err){
+        reject("unable to read file: "+ file);
+    }
+}
+
+//Function to load the contents of our .json files into the array
+//
+module.exports.initialize = function(){
+    return new Promise(
+        (resolve, reject) =>
+        {
+            employees   = readFile('employees', reject);
+            departments = readFile('departments', reject);
+            resolve();
+        }
+    );
+};
+
+// Function to create a promise object
+//      inputs : 
+//          array : request to be formated
+//          funct : function modifies the array (default does nothing)
+//      outputs :
+//          new Promise
+//
+function createPromise(array, funct = (list)=>{ return list; })
+{
+    return new Promise(
+        (resolve, reject) => {
+            array = funct(array);
+            if(array.length == 0) reject("no results returned")
+            else
+                resolve(array);
+        }
+    )
+};
+
+//Function to provide the full array of employee objects
+//
+module.exports.getAllEmployees = function(){
+    return createPromise(employees);
+};
+
+
+//Function to provide the employees which are managers
+//
+module.exports.getManagers = function(){
+    return createPromise(employees, selectManagers);
+};
+
+//Function to select managers from list
+//
+function selectManagers(list){
+    var managers = [];
+    //add employees to the list if they are a manager
+    list.forEach( (data) => { if(data.isManager) managers.push(data);  }  );
+    return managers;
+}
+
+//Function to provide the full array of department objects
+//
+module.exports.getDepartments = function(){
+    return createPromise(departments);
+};
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var path = require("path");
-var app = express();
 
-var onHttpStatus = function(){
-    console.log("Express http server listening on: " + HTTP_PORT );
-};
+var express = require('express');
+var path    = require('path');
+var data    = require('./data-server.js');
+
+var app     = express();
 
 // setup routes to return html files
 //
 app.use(express.static('public'));
 
-// function to setup page requests
+// Function to setup page requests
+//      inputs
+//          str : get location ex /about
+//          file: name of html file
 //
 function createRequest(str, file){
     app.get(str, (req, res)=>{
@@ -22,6 +24,39 @@ function createRequest(str, file){
 createRequest('/', 'home');
 createRequest('/about', 'about');
 
+// Function that creates an app get request
+//      inputs 
+//          str   : get location ex /managers
+//          funct : function call to data-server exported function
+//
+function createGet(str, funct){
+    app.get(str, 
+        (req, res)=>{
+        //call request for data then post to page
+        funct()
+            //return data in form of json array
+            .then((data) => {res.json( data )})
+            //return error in form of json message
+            .catch((err) => {res.json('{message:"'+ err +'"}');});
+        }
+    );
+}
+
+createGet('/managers',    data.getManagers);
+createGet('/employees',   data.getAllEmployees);
+createGet('/departments', data.getDepartments);
+
+// Setup 404 message if page not found
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
 // setup http server to listen on HTTP_PORT
 //
-app.listen(HTTP_PORT, onHttpStatus);
+data.initialize()
+    .then(()=>{app.listen(HTTP_PORT, onHttpStatus)})
+    .catch((err)=>{ console.log(err) });
+
+var onHttpStatus = function(){
+    console.log("Express http server listening on: " + HTTP_PORT );
+};
